24b152fadf79c402f13db8dd663eb8f7230e894a
dieosurya867 <dieosurya@gmail.com>
2022-09-29 23:54:54 +0700

    update sidebar transaksi

--- admin/databuku.php
+++ admin/databuku.php
@@ -112,7 +112,7 @@ include '../function/function.php';
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
                     <i class="fa-solid fa-business-time"></i>
-                    <span>Loan</span>
+                    <span>Transaksi</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
