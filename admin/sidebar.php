f1c39be9cca94131132770d8ca1ac4ddddecbe3b
dieosurya867 <dieosurya@gmail.com>
2022-10-02 16:19:00 +0700

    update & fix tampilan sidebar

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -29,11 +29,11 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
             <i class="fas fa-fw fa-cog"></i>
             <span>Siswa</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Tampilan:</h6>
                 <a class="collapse-item" href="../admin/datasiswa.php">Dashboard Siswa</a>
