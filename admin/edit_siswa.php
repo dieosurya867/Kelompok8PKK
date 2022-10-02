d29d71ff5fbe48e14ae49de0fa0f6edc2b125985
gitakartika25 <teresagita25@gmail.com>
2022-10-02 23:46:57 +0700

    edit siswa udah beres 100%

--- admin/edit_siswa.php
+++ admin/edit_siswa.php
@@ -20,7 +20,9 @@ include '../function/function.php';
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
     <script src="https://kit.fontawesome.com/7b36e01bb8.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet" />
 
     <!-- Custom styles for this template -->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
@@ -60,22 +62,22 @@ include '../function/function.php';
                             <?php
                             if (isset($_GET['nis'])) {
                                 $nis = $_GET["nis"];
-                                $edit = edit("siswa", "nis", "$nis");
+                                $edit = edit_join("siswa, kelas","siswa.id_kelas = kelas.id_kelas", "nis", "$nis");
                                 $data = mysqli_fetch_assoc($edit);
                             }
 
 
                             // Proses update data siswa // Gita Kartika
                             if (isset($_POST['update'])) {
-                                $nis = htmlspecialchars($_POST["nis"]);
+                                $nis = ($_POST["nis"]);
                                 $nama = $_POST['nama'];
                                 $jeniskelamin = $_POST['jeniskelamin'];
                                 $alamat = $_POST['alamat'];
                                 $kelas = $_POST['kelas'];
 
-                                $value = "nama='$nama', jenis_kelamin='$jeniskelamin', alamat='$alamat', id_kelas='$kelas'";
+                                $value = "nama='$nama',jenis_kelamin='$jeniskelamin',alamat='$alamat',id_kelas='$kelas'";
                                 $cekquery = update("siswa", $value, "nis", $nis);
-                                // var_dump($cekquery); die;
+                                //var_dump($value); die;
 
                                 if ($cekquery) {
                                     echo "<div class='alert alert-info'> Data berhasil diupdate.</div>";
@@ -88,41 +90,46 @@ include '../function/function.php';
                             <form action="" method="POST">
                                 <div class="mb-3">
                                     <label for="" class="form-label">NIS</label>
-                                    <input type="text" class="form-control" name="nis" value="<?php echo $data['nis'] ?>" readonly>
+                                    <input type="text" class="form-control" name="nis"
+                                        value="<?php echo $data['nis'] ?>" readonly>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class="form-label">Nama</label>
-                                    <input type="text" class="form-control" name="nama" id="" value="<?php echo $data['nama'] ?>">
+                                    <input type="text" class="form-control" name="nama" id=""
+                                        value="<?php echo $data['nama'] ?>">
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class="form-label">Jenis Kelamin</label> <br>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="jeniskelamin" id="inlineRadio1" value="L" <?php echo ($data['jenis_kelamin'] == 'L') ? 'checked' : '' ?>>
+                                        <input class="form-check-input" type="radio" name="jeniskelamin"
+                                            id="inlineRadio1" value="L"
+                                            <?php echo ($data['jenis_kelamin'] == "L") ?'checked':'' ?>>
                                         <label class="form-check-label" for="inlineRadio1">Laki - laki</label>
                                     </div>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="jeniskelamin" id="inlineRadio2" value="P" <?php echo ($data['jenis_kelamin'] == 'P') ? 'checked' : '' ?>>
+                                        <input class="form-check-input" type="radio" name="jeniskelamin"
+                                            id="inlineRadio2" value="P"
+                                            <?php echo ($data['jenis_kelamin'] == "P") ?'checked':''?>>
                                         <label class="form-check-label" for="inlineRadio2">Perempuan</label>
                                     </div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class="form-label">Alamat</label>
-                                    <input type="text" class="form-control" name="alamat" id="" value="<?php echo $data['alamat'] ?>">
+                                    <input type="text" class="form-control" name="alamat" id=""
+                                        value="<?php echo $data['alamat'] ?>">
                                 </div>
                                 <div class="mb-3">
 
                                     <label for="pilihkelas">Kelas</label>
                                     <select class="form-control" name="kelas">
-                                        <!-- <option selected="<?php echo $data['id_kelas'] > $data['nama_kelas']; ?>"></option> -->
-
                                         <?php
-                                        echo "<option selected =$data[id_kelas]>$data[id_kelas]</option>";
+                                        echo "<option value =$data[id_kelas]>$data[nama_kelas]</option>";
                                         $get_data = read('kelas', 'id_kelas');
                                         while ($data = mysqli_fetch_array($get_data)) {
                                         ?>
-                                            <option value="<?php echo $data['id_kelas']; ?>">
-                                                <?php echo $data['id_kelas'] . " " . $data['nama_kelas']; ?>
-                                            </option>
+                                        <option value="<?php echo $data['id_kelas']; ?>">
+                                            <?php echo $data['nama_kelas']; ?>
+                                        </option>
                                         <?php
                                         }
                                         ?>
@@ -162,7 +169,8 @@ include '../function/function.php';
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
