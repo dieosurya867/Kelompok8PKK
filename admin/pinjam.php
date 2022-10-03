3a604dc3d063363f3f9cd574e71ef3765a4e2812
gitakartika25 <teresagita25@gmail.com>
2022-10-03 13:47:53 +0700

    nambah button detail peminjaman

--- admin/pinjam.php
+++ admin/pinjam.php
@@ -116,9 +116,10 @@ if (isset($_GET['id_peminjaman'])) {
                                                 <td><?= $data['nama'] ?></td>
                                                 <td><?= $data['tanggal_peminjaman'] ?></td>
                                                 <td><?= $data['tanggal_pengembalian'] ?></td>
-                                                <td colspan="2">
+                                                <td colspan="3">
                                                     <a href='edit_peminjaman.php?id_peminjaman=<?php echo htmlspecialchars($data['id_peminjaman']); ?>' class="fa-solid fa-pen-to-square fa-xs btn btn-sm btn-primary" role="button"></a>
                                                     <a href='pinjam.php?id_peminjaman=<?php echo htmlspecialchars($data['id_peminjaman']); ?>' class="fa-solid fa-trash-can btn btn-sm btn-danger" role="button" onclick="return confirm('Are you sure want to delete this?')"></a>
+                                                    <a href='detailpinjam.php?id_peminjaman=<?php echo htmlspecialchars($data['id_peminjaman']); ?>' class="fa-solid fa-book btn btn-sm btn-warning" role="button" ></a>
                                                 </td>
                                             </tr>
                                         <?php
